bd07e1f96974eccee2c411be5dbf0c6b34597169
sharkgua <benoit.george@ubirch.com>
2020-03-23 17:03:24 +0100

    Updated scalatra from 2.6.5 to 2.7.0

--- server/src/main/scala/com/ubirch/webui/server/rest/ApiSwagger.scala
+++ server/src/main/scala/com/ubirch/webui/server/rest/ApiSwagger.scala
@@ -1,7 +1,7 @@
 package com.ubirch.webui.server.rest
 
 import org.scalatra.ScalatraServlet
-import org.scalatra.swagger.{ ApiInfo, NativeSwaggerBase, Swagger }
+import org.scalatra.swagger._
 
 class ResourcesApp(implicit val swagger: Swagger) extends ScalatraServlet with NativeSwaggerBase
 
@@ -9,9 +9,11 @@ object RestApiInfo extends ApiInfo(
   "The Ubirch web admin API",
   "Docs for the Ubirch web admin API",
   "https://ubirch.de",
-  "responsibleperson@ubirch.com",
-  "Apache V2",
-  "https://www.apache.org/licenses/LICENSE-2.0"
+  ContactInfo("Ubirch team", "ubirch.com", "responsibleperson@ubirch.com"),
+  LicenseInfo(
+    "Apache V2",
+    "https://www.apache.org/licenses/LICENSE-2.0"
+  )
 )
 
 class ApiSwagger extends Swagger(Swagger.SpecVersion, "0.1.0", RestApiInfo)
